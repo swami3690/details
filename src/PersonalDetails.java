6be8e2d2557272ff35cd0cd01ab2a9cd308a563b
swami3690 <arpi.derasari09@gmail.com>
2019-07-30 20:34:24 +0100

    first commit

---
+++ src/PersonalDetails.java
@@ -0,0 +1,17 @@
+public class PersonalDetails {
+    public static void main(String args[]){
+        String name = "Arpi";
+        int age = 42;
+        String address = "149,st.pauls avenue, Harrow,Ha3 9pt";
+                String mobile = "07936168167";
+                String salary ="28000";
+System.out.println("My Details");
+System.out.println("name:" +name);
+System.out.println("age:" +age);
+System.out.println("Address:" + address);
+System.out.println("Moblie:" +mobile);
+System.out.println("Salary:" +salary);
+
+
+    }
+}
